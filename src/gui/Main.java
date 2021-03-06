d4f2501d8f7fcb605ebf7f41cbd77d485333a2ea
jjesu <jjesu@LAPTOP-40F3KJGL>
2021-03-05 22:43:42 -0600

    version 0.1

---
+++ src/gui/Main.java
@@ -0,0 +1,43 @@
+/**alimno: Juan Jesus Trejo Gonzalez
+ * matricula:222330
+ */
+package gui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class Main{
+    
+   
+    public static void main(String [] args){
+    
+       JFrame frame = new JFrame();
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        frame.setTitle("Cesba");
+        
+        frame.setResizable(true);
+      //formato default  
+        frame.getContentPane().setBackground(Color.cyan);
+        
+    }
+    
+}
+        
+
+        
+
+        
+
+    
+        
+       
+   
+    
+
+        
+     
+    
+      
+            
+
